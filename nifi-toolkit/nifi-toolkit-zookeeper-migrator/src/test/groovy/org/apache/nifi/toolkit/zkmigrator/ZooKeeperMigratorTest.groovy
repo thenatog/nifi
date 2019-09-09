d783d0244a51b505dc989f477ecb8aef8b687005
thenatog <thenatog@gmail.com>
2019-09-09 12:02:45 -0400

    Upgraded Zookeeper and Curator versions for zookeeper migrator in toolkit. Updated tests to allow for new znode /zookeeper/config

--- nifi-toolkit/nifi-toolkit-zookeeper-migrator/src/test/groovy/org/apache/nifi/toolkit/zkmigrator/ZooKeeperMigratorTest.groovy
+++ nifi-toolkit/nifi-toolkit-zookeeper-migrator/src/test/groovy/org/apache/nifi/toolkit/zkmigrator/ZooKeeperMigratorTest.groovy
@@ -25,6 +25,7 @@ import org.apache.zookeeper.WatchedEvent
 import org.apache.zookeeper.ZKUtil
 import org.apache.zookeeper.ZooDefs
 import org.apache.zookeeper.ZooKeeper
+import org.apache.zookeeper.data.ACL
 import org.apache.zookeeper.data.Stat
 import org.apache.zookeeper.server.auth.DigestAuthenticationProvider
 import spock.lang.Ignore
@@ -245,26 +246,41 @@ class ZooKeeperMigratorTest extends Specification {
         '127.0.0.1,127.0.0.2:2182,127.0.0.3:2183/path/node' | '/path/node' | '127.0.0.1,127.0.0.2:2182,127.0.0.3:2183' || _
     }
 
-    def "Test ignore source"() {
+    def "Test ignore source" () {
         given:
         def server = new TestingServer()
+        def migrationPathRoot = '/nifi/components'
         def connectString = "$server.connectString"
         def dataPath = 'target/test-data-ignore-source.json'
+        def client = new ZooKeeper(server.connectString, 3000, { WatchedEvent watchedEvent ->
+        })
+
+        // Create some znodes under /nifi/components
+        ZKPaths.mkdirs(client, migrationPathRoot)
+        client.setData(migrationPathRoot, 'some data'.bytes, 0)
+        def componentName = '1'
+        client.create("$migrationPathRoot/$componentName", 'some data'.bytes, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT)
+        componentName = '1/a'
+        client.create("$migrationPathRoot/$componentName", 'some data'.bytes, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL)
+        componentName = '2'
+        client.create("$migrationPathRoot/$componentName", 'some data'.bytes, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT)
+        componentName = '3'
+        client.create("$migrationPathRoot/$componentName", 'some data'.bytes, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT)
 
         when: "data is read from the source zookeeper"
-        ZooKeeperMigratorMain.main(['-r', '-z', connectString, '-f', dataPath] as String[])
+        ZooKeeperMigratorMain.main(['-r', '-z', "$connectString$migrationPathRoot", '-f', dataPath] as String[])
 
         then: "verify the data has been written to the output file"
         new File(dataPath).exists()
 
         when: "data is sent to the same zookeeper as the the source zookeeper without ignore source"
-        ZooKeeperMigratorMain.main(['-s', '-z', connectString, '-f', dataPath] as String[])
+        ZooKeeperMigratorMain.main(['-s', '-z', "$connectString$migrationPathRoot", '-f', dataPath] as String[])
 
         then: "verify that an illegal argument exception is thrown"
         thrown(IllegalArgumentException)
 
-        when: "data is sent to the same zookeeper as the source zookeeper with ignore source option is set"
-        ZooKeeperMigratorMain.main(['-s', '-z', connectString, '-f', dataPath, '--ignore-source'] as String[])
+        when: "data is sent to the same zookeeper as the source zookeeper with ignore source option set"
+        ZooKeeperMigratorMain.main(['-s', '-z', "$connectString$migrationPathRoot", '-f', dataPath, '--ignore-source', '--use-existing-acl'] as String[])
 
         then: "no exceptions are thrown"
         noExceptionThrown()
